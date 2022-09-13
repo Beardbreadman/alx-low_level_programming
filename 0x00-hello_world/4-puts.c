3cd386ee90786ef72409edad7bcffcf42ae0be38
beardbreadman <adebayowondeful@gmail.com>
2022-09-13 04:40:16 -0700

    task 4.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+/**
+  *  * main - Entry point
+  *  	*
+  *  	 * Return: always 0
+  */
+#include <stdio.h>
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
