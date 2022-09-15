f520a389884c924926a07477bc28e0bacdc570c7
beardbreadman <adebayowondeful@gmail.com>
2022-09-15 01:20:34 -0700

    task 34.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *
+ * Main -print programming is like biulding a multilingual puzzle 
+ *
+ * Description: prints 'Programming is like building a multilingual puzzle'
+ *
+ * Return: Always 0   (success)
+ */
+
+int main(void)
+
+{
+
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+
+}
