df2ad49bac6789cc81032cfc88cb7aafc1841b40
beardbreadman <adebayowondeful@gmail.com>
2022-09-16 12:43:51 -0700

    tasks 0

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * Main -print quote
+ *
+ * Description: prints Dora's quote
+ *
+ * Return: Always 1 (success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korar, 2015-10-19\n"' 59);
+	return (1);
+}
