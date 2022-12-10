0b351bca666536cfbe3fa2c8dcef761dd39e0b7b
beardbreadman <adebayowondeful@gmail.com>
2022-12-10 01:32:03 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,13 +9,14 @@
 int main(void)
 
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
-
-	for (i = 0; i < 26; i++)
+	char ch = "a;
+	
+	while (ch <= 'z')
 	{
-		putchar(alp[i]);
+		putchar(ch);
+		++ch;
 	}
 	putchar('\n');
+	
 	return (0);
 }
