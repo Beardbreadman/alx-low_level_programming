eb4949c05b2983b6ebd97c5be8e5b56a74bc5073
beardbreadman <adebayowondeful@gmail.com>
2022-12-10 01:41:25 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,18 +2,19 @@
 
 /**
  * main - print numbers from 0 to 9
- *
+ * Description: print all single digit numbers
  * Return: Always 0
  */
 
 int main(void)
 
 {
-	int i;
+	int digit = 0;
 
-	for (i = 0; i < 10; i++);
+	while (digit <= 9)
 	{
-		printf("%d", i);
+		printf("%i", digit);
+		++digit;
 	}
 	putchar('\n');
 	return (0);
