b73c0e2047048abd1d6dfa727953958ed2cb07d7
beardbreadman <adebayowondeful@gmail.com>
2022-12-10 03:33:46 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,12 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char);
+/** 
+ * main - header file
+ * Description: containing declaration for all functions used on this project
+*/
+
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
