37aed2b555d98ca6d9f735a366bdddef8d197632
beardbreadman <adebayowondeful@gmail.com>
2022-12-14 21:58:47 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * main - texts function that prints if integer is positive
- * or negative
+ * main - texts function that prints if integer is + or -
  *Return: 0
  */
 
@@ -11,7 +10,7 @@ int main(void)
 	int i;
 
 	i = 0;
-	
 	positive_or_negative(i);
+	
 	return (0);
 }
