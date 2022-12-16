cc33924a97876637a88f5c226345972b980f940e
beardbreadman <adebayowondeful@gmail.com>
2022-12-16 01:16:57 +0100

    Update 5-more_numbers.c
    number factor

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,31 +1,23 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * more_numbers - print 0 - 14
+ * more_numbers - nmbr fct
  *
- * Retun: void
+ * Return: no return
  */
 
 void more_numbers(void)
-
 {
-	char n, c;
-	int i = 0;
-	while (i < 10)
-	{
-		for (n = 0; n <= 14; n++)
-		{
-			c = n;
-			if (n > 9)
-			{
-				_putchar('1');
-				c = n % 10;
-			}
-			_putchar('0' + c);
-		}
-		_putchar('\n');
-		i++;
-	}
-}
 
+int i, j;
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			_putchar('1');
+			_putchar(j % 10 + '0');
+		}
+			_putchar('\n');
+		}
+}
