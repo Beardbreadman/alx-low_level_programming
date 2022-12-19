f0fa4ff1fc6671cf57caf38eeb5b2047c4721ba6
Wonderful Emmanuel <adebayowondeful@gmail.com>
2022-12-20 00:06:03 +0100

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,24 +1,36 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * _strrcpy - copies the string pointed to by src, plus 
- * the terminating null byte, to the buffer pointed to 
- * by dest
- * @dest: destination
- * @src: source
- * Return: the pointer to dest
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
  */
-char *_strcpy(char *dest, char *src)
-{
-	int count = 0;
 
-	while (count >= 0)
+int main(void)
+{
+	int x, sum, y;
+	int pass[100];
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (x = 0; x < 100; x++)
 	{
-		*(dest + count) = *(src + count);
-		if (*(src + count) == '\0')
+		pass[x] = rand() % 78;
+		sum += (pass[x] + '0');
+		putchar(pass[x] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			y = 2772 - sum - '0';
+			sum += y;
+			putchar(y + '0');
 			break;
-		count++;
+		}
 	}
-	return (dest);
+
+	return (0);
 }
