99fb5251ffcaef66ea0fb0a1df9d5ffdc2c753d3
Wonderful Emmanuel <adebayowondeful@gmail.com>
2022-12-20 06:08:16 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,29 +6,21 @@
  * @s: input stringto be capitalised
  * Return: the pointer to destination
  */
-
 char *cap_string(char *s)
-
 {
-	int count = 0, i;
-	int sep_words[] = {32, 44, 46, 33,63,59, 9, 34, 40, 123, 125, 10, 41};
-
-	if (*(s + count) >= 97 && *(s + count) <= 122)
-		*(s + count) = *(s + count) - 32;
-	count++;
-	while (*(s + count) != '\0')
-	{
-		for (i = 0; i < 13; i++)
-		{
-			if (*(s + count) == sep_words[i])
-			{
-				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-					*(s + (count + 1)) = *(s + (count + 1)) - 32;
-				break;
-			}
-		}
-		count++;
-	}
-	return (s);
+int a = 0, i;
+int cspc = 13;
+char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+while (s[a])
+{
+i = 0;
+while (i < cspc)
+{
+if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
+s[a] -= 32;
+i++;
 }
-			
+a++;
+}
+return (s);
+}
