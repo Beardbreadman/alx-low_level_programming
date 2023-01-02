d7721052dda520f67e1c0d37ecfab0c1e647d407
Wonderful Emmanuel <adebayowondeful@gmail.com>
2023-01-02 14:12:58 +0100

    Update 0-main.c

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -1,8 +1,5 @@
 #include "main.h"
-
 #include <stdio.h>
-
-
 
 /**
  * simple_print_buffer - prints buffer in hexa
@@ -13,46 +10,26 @@
  */
 
 void simple_print_buffer(char *buffer, unsigned int size)
-
 {
-
         unsigned int i;
-
-
-
         i = 0;
 
         while (i < size)
-
         {
-
                 if (i % 10)
-
                 {
-
                         printf(" ");
-
                 }
-
                 if (!(i % 10) && i)
-
                 {
-
                         printf("\n");
-
                 }
-
                 printf("0x%02x", buffer[i]);
 
                 i++;
-
-        }
-
-        printf("\n");
-
+                }
+                 printf("\n");
 }
-
-
 
 /**
  * main - check the code
@@ -61,21 +38,12 @@ void simple_print_buffer(char *buffer, unsigned int size)
  */
 
 int main(void)
-
 {
-
     char buffer[98] = {0x00};
-
-
-
     simple_print_buffer(buffer, 98);
-
     _memset(buffer, 0x01, 95);
-
     printf("-------------------------------------------------\n");
-
     simple_print_buffer(buffer, 98);    
 
     return (0);
-
 }
